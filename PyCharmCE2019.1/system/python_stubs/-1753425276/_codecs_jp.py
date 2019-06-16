e6007468a04b9841c028a0dd55a06790868ed9bc
Mono <mkobrocka@gmail.com>
2019-06-16 21:12:35 +0200

    new dot files

---
+++ PyCharmCE2019.1/system/python_stubs/-1753425276/_codecs_jp.py
@@ -0,0 +1,41 @@
+# encoding: utf-8
+# module _codecs_jp
+# from /home/mono/anaconda3/envs/twitter/lib/python3.6/lib-dynload/_codecs_jp.cpython-36m-x86_64-linux-gnu.so
+# by generator 1.147
+# no doc
+# no imports
+
+# functions
+
+def getcodec(*args, **kwargs): # real signature unknown
+    pass
+
+# no classes
+# variables with complex values
+
+__loader__ = None # (!) real value is '<_frozen_importlib_external.ExtensionFileLoader object at 0x7f1bc7d4c668>'
+
+__map_cp932ext = None # (!) real value is '<capsule object "multibytecodec.__map_*" at 0x7f1bc7d7b060>'
+
+__map_jisx0208 = None # (!) real value is '<capsule object "multibytecodec.__map_*" at 0x7f1bc7c93ea0>'
+
+__map_jisx0212 = None # (!) real value is '<capsule object "multibytecodec.__map_*" at 0x7f1bc7c93e40>'
+
+__map_jisx0213_1_bmp = None # (!) real value is '<capsule object "multibytecodec.__map_*" at 0x7f1bc7c93e10>'
+
+__map_jisx0213_1_emp = None # (!) real value is '<capsule object "multibytecodec.__map_*" at 0x7f1bc7c93f60>'
+
+__map_jisx0213_2_bmp = None # (!) real value is '<capsule object "multibytecodec.__map_*" at 0x7f1bc7c93ed0>'
+
+__map_jisx0213_2_emp = None # (!) real value is '<capsule object "multibytecodec.__map_*" at 0x7f1bc7c93f90>'
+
+__map_jisx0213_bmp = None # (!) real value is '<capsule object "multibytecodec.__map_*" at 0x7f1bc7c93f30>'
+
+__map_jisx0213_emp = None # (!) real value is '<capsule object "multibytecodec.__map_*" at 0x7f1bc7c93fc0>'
+
+__map_jisx0213_pair = None # (!) real value is '<capsule object "multibytecodec.__map_*" at 0x7f1bc7d7b030>'
+
+__map_jisxcommon = None # (!) real value is '<capsule object "multibytecodec.__map_*" at 0x7f1bc7c93f00>'
+
+__spec__ = None # (!) real value is "ModuleSpec(name='_codecs_jp', loader=<_frozen_importlib_external.ExtensionFileLoader object at 0x7f1bc7d4c668>, origin='/home/mono/anaconda3/envs/twitter/lib/python3.6/lib-dynload/_codecs_jp.cpython-36m-x86_64-linux-gnu.so')"
+
